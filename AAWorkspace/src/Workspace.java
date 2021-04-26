0ac040ce8cb02c87f70028a23c2395ffae7945f9
fisherwyattd <67201849+fisherwyattd@users.noreply.github.com>
2021-04-26 19:30:52 -0400

    Update from desktop files

--- AAWorkspace/src/Workspace.java
+++ AAWorkspace/src/Workspace.java
@@ -1,48 +1,29 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Random;
 import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 public class Workspace {
-    private static int numberOfYes = 0;
-    private static final long NUMBER_OF_TRIALS = 10000000L;
 
-
-    public static void pickBalls() {
-        int numberOfBlue = 3;
-
-        Random rand = new Random();
-
-        int result1 = rand.nextInt(12) + 1;
-        int result2 = rand.nextInt(12) + 1;
-
-        while(result1 == result2) {
-            result2 = rand.nextInt(12) + 1;
-        }
-        
-        if((result1 > numberOfBlue) && (result2 > numberOfBlue)) { numberOfYes++; }
-    }
-
-    public static void getProbability() {
-        numberOfYes = 0;
-
-        for(int i = 0; i < NUMBER_OF_TRIALS; i++) {
-            Workspace.pickBalls();
-        }
-
-        System.out.println("Probability of yes: " + ((double)numberOfYes / NUMBER_OF_TRIALS));
-    }
 
     public static void main(String[] args){
-        getProbability();
-        getProbability();
-        getProbability();
-        getProbability();
-        getProbability();
-        getProbability();
-        getProbability();
-        getProbability();
-        getProbability();
-        getProbability();
+        File inFile = new File("C:\\Users\\fishe\\Desktop\\CTF\\50k-users.txt");
+        Scanner fileScan;
+        try {
+             fileScan = new Scanner(inFile);
+            while(fileScan.hasNext()) {
+                String username = fileScan.nextLine();
+                if(username.charAt(2) == 'x') {
+                    if(username.charAt(3) == '2' | username.charAt(3) == '3' | username.charAt(3) == '4' |
+                            username.charAt(3) == '5' | username.charAt(3) == '6') {
+                        System.out.println(username);
+                    }
+                }
+            }
+        } catch(FileNotFoundException e){
+            System.out.print("File not found.");
+        }
 
 
     }
